e90e2ae85cc973e9a044c05656f73f8b7cfd991c
MUSQUITU <camilinhaalmeida55@gmail.com>
2024-01-19 22:00:34 -0300

    :white_check_mark: Add stop countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Play } from 'phosphor-react'
+import { HandPalm, Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
@@ -11,6 +11,7 @@ import {
   MinutesAmountInput,
   Separator,
   StartCountdownButton,
+  StopCountdownButton,
   TaskInput,
 } from './styles'
 import { useEffect, useState } from 'react'
@@ -45,9 +46,9 @@ export function Home() {
       minutesAmount: 0,
     },
   })
-  // ciclo ativo
+  //! ciclo ativo
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
-  // redução do countdown
+  //* redução do countdown
   useEffect(() => {
     let interval: number
 
@@ -81,7 +82,7 @@ export function Home() {
     reset()
   }
 
-  // se tiver um ciclo ativo, essa variável será o numero de minutos *60, se não tiver um ciclo o valor será zero.
+  // * se tiver um ciclo ativo, essa variável será o numero de minutos *60, se não tiver um ciclo o valor será zero.
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   // contagem de quantos segundos se passaram
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
@@ -91,7 +92,7 @@ export function Home() {
   // quantos segundos restou da divisão
   const secondsAmount = currentSeconds % 60
 
-  // preencher o tamanho especifico, obrigatorio ter 2caracteres, se não inclui o 0 no inicio
+  // * preencher o tamanho especifico, obrigatorio ter 2caracteres, se não inclui o 0 no inicio
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
 
@@ -114,6 +115,7 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            disabled={!!activeCycle}
             {...register('task')}
           />
 
@@ -130,6 +132,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            disabled={!!activeCycle} //* (!!) converte para boolean
             {...register('minutesAmount', { valueAsNumber: true })}
           />
 
@@ -144,10 +147,17 @@ export function Home() {
           <span>{seconds[1]}</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
-          <Play size={24} />
-          Começar
-        </StartCountdownButton>
+        {activeCycle ? (
+          <StopCountdownButton type="button">
+            <HandPalm size={24} />
+            Interromper
+          </StopCountdownButton>
+        ) : (
+          <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountdownButton>
+        )}
       </form>
     </HomeContainer>
   )
